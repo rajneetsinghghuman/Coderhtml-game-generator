a5c8d84148ae535afc0060e53a0d72acc56751b8
rajneetsinghghuman <144692621+rajneetsinghghuman@users.noreply.github.com>
2023-09-11 19:16:45 +0530

    Update Coderhtml game genarator.py

--- Coderhtml game genarator.py
+++ Coderhtml game genarator.py
@@ -1,4 +1,4 @@
-#Code power game maker
+#Codehtml game generator
 #Type of device for the game. One space in python for indentation which is used for brackets in other programming languages. Use one space in python.
 print("Type the option content and press the enter key.")
 gametype= input("Which type of game- 1) PC 2) Mobile-")
